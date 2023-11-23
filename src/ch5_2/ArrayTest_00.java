60927f4104c2f179bd1623dd03aef6844e0670e4
won15367 <won15367@gmail.com>
2023-11-23 10:42:52 +0900

    배열 test

---
+++ src/ch5_2/ArrayTest_00.java
@@ -0,0 +1,13 @@
+package ch5_2;
+
+public class ArrayTest_00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {80, 70, 50, 60};
+		// 배열에 들어가는 타입[] 변수명
+		int[] arr1 = new int[5]; 
+		//배열은 선언할때 몇개의 원소를 갖는지 알려줘야된다.
+	}
+
+}
---
+++ src/ch5_2/ArrayTest_01.java
@@ -0,0 +1,21 @@
+package ch5_2;
+
+public class ArrayTest_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] scores = {90, 85, 75, 80};
+		
+		for (int i=0;i<scores.length;i++) {
+			System.out.println(scores[i]);
+		}
+
+		int[] arr1 = new int[10];		
+		
+		for (int i=0;i<10;i++) {
+			System.out.println(arr1[i]);
+		}
+	}
+
+}
