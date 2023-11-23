b63e7d7274309e201d9cd0a531899e567b77ce6a
won15367 <won15367@gmail.com>
2023-11-23 12:36:41 +0900

    향상된 for문

---
+++ src/ch5_2/UpgradeFor_00.java
@@ -0,0 +1,19 @@
+package ch5_2;
+
+public class UpgradeFor_00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr1 = {1,2,3,4};
+		int sum = 0;
+		
+		for (int num:arr1) {
+			sum += num;
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
